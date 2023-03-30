1608ff731650a7767de6a2678a8306ba40c0f80b
kot <talalaev.misha@gmail.com>
2023-03-31 01:50:34 +0400

    add startCells add fnc createBoard add fnc addXorO add style.css

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const gameBoard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
 
+const startCells = [
+  '', '', '',
+  '', '', '',
+  '', '', ''
+]
+
+function createBoard () {
+startCells.forEach ((cell,index) => {
+  const cellElement = document.createElement ("div")
+  cellElement.classList.add("cell")
+  cellElement.id = index
+  cellElement.addEventListener("click", addXorO)
+  gameBoard.append(cellElement)
+})}
+
+createBoard ()
+
+function addXorO (e) {
+  const display = document.createElement("div")
+  display.classList.add("circle")
+  e.target.append(display)
+}
